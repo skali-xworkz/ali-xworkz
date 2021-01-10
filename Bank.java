51c7df7851b12c4a38130f3c7071ee350e5a3c63
skali-xworkz <alishaik.xworkz@gmail.com>
2021-01-09 20:59:02 -0800

    Adding Method programs

---
+++ Bank.java
@@ -0,0 +1,11 @@
+class Bank
+{
+   public static void main(String a[])
+   {
+   bankService();
+   }
+   static void bankService()
+   {
+   System.out.println("It provides HOME Loan, Education Loan");
+   }
+}
---
+++ Factorial.java
@@ -0,0 +1,19 @@
+class Factorial
+{
+public static void main(String a[])
+{
+int $num=5;
+  fact($num);
+}
+static void fact(int $num)
+{
+int f=1;
+ for(int i=1; i<=$num;i++)
+ {
+ f=f*i;
+System.out.println(i);
+
+ }
+ 
+}
+}
