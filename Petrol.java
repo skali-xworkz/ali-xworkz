acf288d152b307674b7435f81a0e89a72406e2e5
skali-xworkz <alishaik.xworkz@gmail.com>
2021-01-15 11:39:26 +0530

    adding test file

---
+++ Petrol.java
@@ -0,0 +1,14 @@
+class Petrol
+{
+	public static void main(String a[])
+	{
+		String fueltype1="Normal";
+		String fueltype2="power";
+		int pricen=100;
+		int pricepo=200;
+		 System.out.println(fueltype1);
+		 System.out.println(fueltype2);
+		 System.out.println(pricen);
+		 System.out.println(pricepo);
+		 }
+}
