c5e91632411c69ed4a55fa72649eff2ad035ae19
skali-xworkz <alishaik.xworkz@gmail.com>
2021-01-15 11:30:11 +0530

    adding private files

---
+++ GardenTester.java
@@ -0,0 +1,13 @@
+class GardenTester
+{
+public static void main(String a[])
+{
+	GardenDTO dto=new GardenDTO();
+	System.out.println("****************************");
+	dto.setName("bharathi garden");
+	dto.setAddress("jayanagar");
+	dto.type;
+	System.out.println(dto.getName()+"\t"+dto.getAddress());//"\t"+GardenDTO.type);
+	System.out.println("****************************");
+}
+}
