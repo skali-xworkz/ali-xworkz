06e1dc2f35fd611368885821681befce384f0cfe
skali-xworkz <76859844+skali-xworkz@users.noreply.github.com>
2021-03-18 08:31:02 -0700

    FIRE EXCEPTION UPDATED TODAY

---
+++ fireexception/src/com/xworkz/xworkzapp/custom/InValidVehicle.java
@@ -0,0 +1,9 @@
+package com.xworkz.xworkzapp.custom;
+
+public class InValidVehicle extends RuntimeException {
+	public InValidVehicle(String exceptionoccured){ 
+		// TODO Auto-generated constructor stub
+		super(exceptionoccured);
+
+}
+}
---
+++ fireexception/src/com/xworkz/xworkzapp/service/FireService.java
@@ -0,0 +1,7 @@
+package com.xworkz.xworkzapp.service;
+
+import com.xworkz.xworkzapp.dto.FireDTO;
+
+public interface FireService {
+public void validId(FireDTO dto);
+}
