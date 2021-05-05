cf01be95ce2a9602b88b7f2502d37a44bb24e60d
skali-xworkz <76859844+skali-xworkz@users.noreply.github.com>
2021-05-05 05:12:32 -0700

    Using delete functions

---
+++ river-jdbc/java/com/xworkz/river/RiverDelete.java
@@ -0,0 +1,34 @@
+package com.xworkz.river;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class RiverDelete {
+	public static void main(String[] args) throws ClassNotFoundException,SQLException
+	{
+		//com.mysql.jdbc.driver=version is 5
+		// com.mysql.cj.jdbc.Driver
+		Class.forName("com.mysql.jdbc.Driver");
+		//jdbc:mysql://localhost:3306/xworkz.db
+		
+	Connection conn= 
+	DriverManager.getConnection("jdbc:mysql://localhost:3306/xworkz.db","root","root");
+		if(conn!=null)
+		{
+			
+		System.out.println("conncected to db,can follow next steps");
+		Statement statement=conn.createStatement();
+		String sqlQuery="delete from river where R_ID=5";
+		String sqlQuery1="delete from river where R_NAME='Yamuna'";
+		int delte=statement.executeUpdate(sqlQuery);
+		int delte1=statement.executeUpdate(sqlQuery1);
+		System.out.println("deleted succesfullly "+delte);
+		System.out.println("deleted sucessfully "+delte1);
+		}
+		else{
+		System.out.println("not connected");
+		}
+	}	
+	}
