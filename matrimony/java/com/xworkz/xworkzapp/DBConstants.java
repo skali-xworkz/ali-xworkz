18aa6b1a8b85d8fd673459625f0a595d96978e71
skali-xworkz <76859844+skali-xworkz@users.noreply.github.com>
2021-05-12 06:42:56 +0530

    Delete matrimony/java/com/xworkz/xworkzapp directory

--- matrimony/java/com/xworkz/xworkzapp/DBConstants.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.xworkzapp;
-
-public interface DBConstants {
-	String URL = "jdbc:mysql://localhost:3306/xworkz.db";
-	String USERNAME = "root";
-	String PASSWORD = "root";
-	String DRIVER = "com.mysql.cj.jdbc.Driver";
-}
