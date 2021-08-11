813f291da854eaee35c5be297e707c9f9d6e0fda
skali-xworkz <76859844+skali-xworkz@users.noreply.github.com>
2021-08-11 22:06:31 +0530

    Enroll Mail_done

---
+++ enroll_app_send_Mail_password/src/main/java/com/xworkz/enroll/util/RandomPasswordGenerator.java
@@ -0,0 +1,19 @@
+package com.xworkz.enroll.util;
+
+import java.security.SecureRandom;
+
+public class RandomPasswordGenerator {
+	public static String generateRandomPassword(int len) {
+		// ASCII range – alphanumeric (0-9, a-z, A-Z)
+		final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		SecureRandom random = new SecureRandom();
+		StringBuilder sb = new StringBuilder();
+		// each iteration of the loop randomly chooses a character from the given
+		// ASCII range and appends it to the `StringBuilder` instance
+		for (int i = 0; i < len; i++) {
+			int randomIndex = random.nextInt(chars.length());
+			sb.append(chars.charAt(randomIndex));
+		}
+		return sb.toString();
+	}
+}
