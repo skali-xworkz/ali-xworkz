4d819243537fb7019df3fbfe0b648e2d5b3bad7c
skali-xworkz <76859844+skali-xworkz@users.noreply.github.com>
2021-08-11 12:17:36 +0530

    Delete enroll_app directory

--- enroll_app/src/main/java/com/xworkz/enroll/dao/EnrollDAO.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.enroll.dao;
-
-import com.xworkz.enroll.entity.EnrollEntity;
-
-public interface EnrollDAO {
-public boolean saveEnrollDTO(EnrollEntity enrollEntity);
-}
--- enroll_app/src/main/java/com/xworkz/enroll/service/EnrollService.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.enroll.service;
-
-import com.xworkz.enroll.dto.EnrollDTO;
-
-public interface EnrollService {
-boolean enrollStudent(EnrollDTO dto);
-}
